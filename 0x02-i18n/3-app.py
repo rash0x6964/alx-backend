1e67b27f1fa3d9e53fcf2ba7faae89ea94a4dd89
rash0x6964 <rash0x6964@gmail.com>
2024-08-27 17:10:36 +0100

    task 3: Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -30,7 +30,7 @@ def get_locale() -> str:
 @app.route('/')
 def index() -> str:
     """ Default route """
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == "__main__":
