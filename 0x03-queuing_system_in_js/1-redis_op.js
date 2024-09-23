cdcaa277b38047c9afe687ee27919f3ea11efdc7
rash0x6964 <rash0x6964@gmail.com>
2024-09-23 18:15:09 +0100

    Task 2: Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,25 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('ready', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error(`Redis client not connected to the server: ${err.message}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, object) => {
+    console.log(object);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
